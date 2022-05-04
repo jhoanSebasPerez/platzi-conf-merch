8ad7102875ea96e0f317f7172223964f4a225055
jhoanSebasPerez <jhoansebastianperez73@gmail.com>
2022-05-04 16:51:07 -0500

    add paypal script provider in App

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -11,25 +11,30 @@ import Layout from "../components/Layout";
 import AppContext from "../context/AppContext";
 import useInitialState from "../hooks/useInitialState";
 import initialState from "../initialState";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 function App() {
   const globalState = useInitialState(initialState);
+  const clientId = import.meta.env.VITE_CLIENT_ID_PAYPAL;
 
   return (
-    <AppContext.Provider value={globalState}>
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="checkout" element={<Checkout />} />
-            <Route path="checkout/information" element={<Information />} />
-            <Route path="checkout/payment" element={<Payment />} />
-            <Route path="checkout/success" element={<Success />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </AppContext.Provider>
+    <>
+      <PayPalScriptProvider options={{ "client-id": clientId }} />
+      <AppContext.Provider value={globalState}>
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="checkout" element={<Checkout />} />
+              <Route path="checkout/information" element={<Information />} />
+              <Route path="checkout/payment" element={<Payment />} />
+              <Route path="checkout/success" element={<Success />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </AppContext.Provider>
+    </>
   );
 }
 
